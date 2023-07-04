9f07b4d7a2fb1eadab11b4ede3d7f5086c3271cc
DESKTOP-76H2EFU\kimnamju <nj.kim@softleaf.co.kr>
2023-07-04 12:57:26 +0900

    stream

--- src/chaptor3/addmulti/RunAddMulti.java
+++ src/chaptor3/addmulti/RunAddMulti.java
@@ -4,12 +4,29 @@ import java.util.function.Function;
 
 public class RunAddMulti {
     public static void main(String[] args) {
-        Function<Integer, Integer> f = x -> x + 1;
-        Function<Integer, Integer> g = x -> x * 2;
-        Function<Integer, Integer> integerIntegerFunction = f.andThen(g);
-        Integer apply = integerIntegerFunction.apply(1);
+        Function<Integer, Integer> f = x -> x + 10;
+        Function<Integer, Integer> g = x -> x * 10;
+        Function<Integer, Integer> integerIntegerFunction = f.compose(g);
+        Integer apply = integerIntegerFunction.apply(5);
         Integer apply1 = f.apply(1);
         System.out.println("apply = " + apply);
         System.out.println("apply1 = " + apply1);
+        Function<String, String> addHeader = Letter::addHeader;
+        Function<String, String> then = addHeader.andThen(Letter::checkSpelling)
+                .andThen(Letter::addFooter);
+        System.out.println("then = " + then);
+    }
+}
+
+class Letter {
+    public static String addHeader(String text) {
+        return "From Raoul, Mario and Alan: " + text;
+    }
+
+    public static String addFooter(String text) {
+        return text + "Kind regards";
+    }
+    public static String checkSpelling(String text) {
+        return text.replaceAll("labda", "lambda");
     }
 }
